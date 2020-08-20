9fac8da0a600f1f76bfe0041a1c645edec0d53f0
Hazuki Onohara <neko.aoba.suzukaze@gmail.com>
2020-08-20 09:14:26 +0700

    👴

--- aoba-proxy.py
+++ aoba-proxy.py
@@ -16,19 +16,6 @@ from http.server import HTTPServer
 glob.conf = configHelper.config("config.ini")
 glob.verbose = generalHelper.stringToBool(glob.conf.config["console"]["verbose"])
 
-if glob.conf.default:
-	# We have generated a default config.ini, quit server
-	consoleHelper.printWarning()
-	consoleHelper.printColored("[!] config.ini not found. A default one has been generated.", bcolors.YELLOW)
-	consoleHelper.printColored("[!] Please edit your config.ini and run the server again.", bcolors.YELLOW)
-	sys.exit()
-
-if not glob.conf.checkConfig():
-	consoleHelper.printError()
-	consoleHelper.printColored("[!] Invalid config.ini. Please configure it properly", bcolors.RED)
-	consoleHelper.printColored("[!] Delete your config.ini to generate a default one", bcolors.RED)
-	sys.exit()
-
 def parse_args(argv=sys.argv[1:]):
 	parser = argparse.ArgumentParser(description='Aoba Proxy Server')
 	parser.add_argument('--port', dest='port', type=int, default=32767,
@@ -41,6 +28,20 @@ def main(argv=sys.argv[1:]):
 	args = parse_args(argv)
 	hostname = glob.hostname
 	consoleHelper.printServerStartHeader(True)
+
+	if glob.conf.default:
+		# We have generated a default config.ini, quit server
+		consoleHelper.printWarning()
+		consoleHelper.printColored("[!] config.ini not found. A default one has been generated.", bcolors.YELLOW)
+		consoleHelper.printColored("[!] Please edit your config.ini and run the server again.", bcolors.YELLOW)
+		sys.exit()
+
+	if not glob.conf.checkConfig():
+		consoleHelper.printError()
+		consoleHelper.printColored("[!] Invalid config.ini. Please configure it properly", bcolors.RED)
+		consoleHelper.printColored("[!] Delete your config.ini to generate a default one", bcolors.RED)
+		sys.exit()
+
 	consoleHelper.printColored("{}Proxying {} and hosting on 127.0.0.1:{}...".format(bcolors.UNDERLINE, glob.hostname, args.port), bcolors.GREEN)
 	if glob.verbose:
 		consoleHelper.printColored("WARNING! VERBOSE MODE IS ON!", bcolors.YELLOW)
